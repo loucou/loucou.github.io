1777529d8ce060b15abd752f5961550431472e7c
LC <coulet4@gmail.com>
2020-12-14 15:42:35 +0100

    qsdf

--- script.js
+++ script.js
@@ -3,20 +3,22 @@
 
 	document.addEventListener("DOMContentLoaded", function () {
 		document.querySelectorAll(".flip-card").forEach(function (card) {
-			// in order to be usable on phones:
 			card.addEventListener("click", flipCard(card), false);
-
-			// For PCs, handle with :hover in css
-			// card.addEventListener("mouseenter", flipCard(card), false);
-			// card.addEventListener("mouseleave", flipCard(card), false);
 		});
 	});
 
 	function flipCard(card) {
 		var inner = card.querySelector(".flip-card-inner");
 		return function (event) {
-			var currentTransform = inner.style.transform;
-			inner.style.transform = currentTransform == "" ? "rotateY(180deg)" : "";
+			var cur = inner.style.transform;
+			inner.style.transform = cur != "rotateY(180deg)" ? "rotateY(180deg)" : "";
 		};
+	}
+
+	function log(str) {
+		var output = document.getElementById("output");
+		if (output) {
+			output.innerHTML = output.innerHTML + "\n" + str;
+		}
 	}
 })();
