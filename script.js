a376d3b7396bea27b92f5099bda175c8656c649f
LC <coulet4@gmail.com>
2020-12-14 16:43:51 +0100

    fix for safari

--- script.js
+++ script.js
@@ -4,13 +4,31 @@
 	document.addEventListener("DOMContentLoaded", function () {
 		document.querySelectorAll(".flip-card").forEach(function (card) {
 			card.addEventListener("click", flipCard(card), false);
+			card.addEventListener("mouseenter", flipCard(card), false);
+			card.addEventListener("mouseleave", flipCard(card), false);
 		});
 	});
 
 	function flipCard(card) {
 		var inner = card.querySelector(".flip-card-inner");
 		return function (event) {
-			inner.classList.toggle("is-flipped");
+			var cl = inner.classList;
+
+			if (event.type === "click") {
+				if (cl.contains("is-clicked")) {
+					cl.remove("is-clicked");
+					cl.remove("is-flipped");
+				} else {
+					cl.add("is-clicked");
+					cl.add("is-flipped");
+				}
+			} else if (event.type === "mouseenter") {
+				cl.add("is-flipped");
+			} else if (event.type === "mouseleave") {
+				if (!cl.contains("is-clicked")) {
+					cl.remove("is-flipped");
+				}
+			}
 		};
 	}
 
